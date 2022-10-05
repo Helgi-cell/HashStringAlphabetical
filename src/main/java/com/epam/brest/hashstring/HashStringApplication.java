e6cbadedd7ba31d516c506d7011f27d5e1d22956
Oleg Suhodolsky <olegsuhodolsky@gmail.com>
2022-10-05 18:31:30 +0300

    Created

---
+++ src/main/java/com/epam/brest/hashstring/HashStringApplication.java
@@ -0,0 +1,13 @@
+package com.epam.brest.hashstring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HashStringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HashStringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epam/brest/hashstring/HashStringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.brest.hashstring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HashStringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
